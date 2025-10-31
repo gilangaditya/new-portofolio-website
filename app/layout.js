250738183746d8bde3bbeee5658796546cc41ed1
Gilang Aditya <gilangadtia712@gmail.com>
2025-10-31 13:31:43 +0700

    Add Vercel Analytics for enhanced performance tracking

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Outfit, Ovo } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         className={`${outfit.className} ${ovo.className} antialiased leading-8 over-flow-x-hidden dark:bg-darkTheme dark:text-white` }
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
